99a1e335a9c6d6316390ec331fbc9e46c7059f96
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-10-29 13:44:07 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,16 +4,23 @@ import "dotenv/config";
 const sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-    })
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASS,
-        {
-          host: "localhost",
-          dialect: "postgres",
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true, 
+            rejectUnauthorized: false, 
+          },
         },
+      })
+    : new Sequelize(
+        process.env.DB_NAME,      
+        process.env.DB_USER,
+        process.env.DB_PASS, 
+        {
+          host: process.env.DB_HOST || "localhost",  
+          port: process.env.DB_PORT || 5432,
+          dialect: "postgres",
+        }
       );
 
 export default sequelize;
